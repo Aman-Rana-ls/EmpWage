30a4d3efd678d5a43d6c8d713f58af03dc139a6a
Aman-Rana-ls <suppa0173@gmail.com>
2024-12-18 15:03:56 +0530

    [Aman Rana] :ADDED c# UC1 Attendence File

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Attendence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();  
+
+            int n = rnd.Next(0,2);
+            if (n == 0) {
+                Console.WriteLine("The Employee is Absent");
+            }
+            else
+            {
+                Console.WriteLine("The Employee is Present");
+            }
+            Console.ReadKey();  
+
+        }
+    }
+}
