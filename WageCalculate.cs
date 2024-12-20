065e170bb858709a248f135f64fa86f127e7ee2a
Aman-Rana-ls <suppa0173@gmail.com>
2024-12-20 09:48:29 +0530

    [Aman Rana]:Implemented UC2

---
+++ WageCalculate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace WageCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int n = rnd.Next(0, 2);
+            if (n == 0)
+            {
+                Console.WriteLine("The Employee is Absent.");
+            }
+            else
+            {
+
+                Console.WriteLine("The Employee is Present.");
+                int[] arr = { 0, 4, 8 };
+                int h = arr[rnd.Next(arr.Length)];
+                switch (h)
+                {
+                    case 0: Console.WriteLine("The Employee doesn't earned money today as he didn't worked at all."); break;
+
+                    case 4: Console.WriteLine("The Employee earned 80Rs today as he worked for 4 Hours Half Time."); break;
+
+                    case 8: Console.WriteLine("The Employee earned 160Rs today as he worked for 8 Hours Full Time."); break;
+
+                }
+
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
